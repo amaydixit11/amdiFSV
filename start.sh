b37478b67ce652f59a3c060d89682afbc3f37d9e
amaydixit11 <amaydixit11@gmail.com>
2024-11-17 11:07:17 +0530

    initialized tauri

---
+++ start.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+export LD_LIBRARY_PATH=/opt/glibc-2.36/lib ./amdi-fsv/src-tauri/target/debug/app
+# LD_LIBRARY_PATH=/opt/glibc-2.36/lib ./Desktop/Dev/amdi-fsv/src-tauri/target/debug/app
